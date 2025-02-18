e292e04b30de016695623f300668e9de8984e20c
Konstantin Sotnikov <108389618+konstantin-sotnikov-clearscale@users.noreply.github.com>
2025-02-18 14:40:58 -0500

    Update provider.tf

--- test/terraform/provider.tf
+++ test/terraform/provider.tf
@@ -2,12 +2,17 @@
 # Specify which provider(s) this module requires.
 # https://developer.hashicorp.com/terraform/language/providers/configuration
 #
+terraform {
+  required_version = ">= 1.5.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   max_retries = 3
   region      = "us-east-1"
 }
-
-#
-# Current AWS context
-#
-data "aws_caller_identity" "current" {}
